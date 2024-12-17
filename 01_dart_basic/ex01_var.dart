a388626563670d580f24555f9c72689bd91394ed
brixon <brixon@naver.com>
2024-12-17 13:03:05 +0900

    day01

---
+++ 01_dart_basic/ex01_var.dart
@@ -0,0 +1,18 @@
+void main() {
+    // var
+    var name = '홍길동';
+    var name2 = '임꺽정';
+    var age = 10;
+  
+    print(name);
+    print(name2);
+  
+    // 같은 이름으로 두번 선언 할수 없다.
+  
+    name = '둘리';
+  //  print( "name = " + age);
+  
+    print( "name = ${age}" ); 
+    print( "name = $age" );
+  
+}
